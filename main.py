9a7f452ef3fcaba6e96ec548ccd565054df67f99
francesco toscano <francesco.toscano@itas.ca>
2025-05-01 15:33:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     url = f"https://{shop_domain}/admin/api/2024-01/products.json?limit=50"
 
     try:
-        response = requests.get(url, headers=headers, verify=certifi.where())
+        response = requests.get(url, headers=headers, verify=False)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"❌ API request failed: {e}")
